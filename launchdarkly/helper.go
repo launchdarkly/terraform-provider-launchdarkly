dbb08e8064f99eaf7b9f0a9a7fd869f51194ec13
drichelson <drichelson@gmail.com>
2019-02-13 21:38:01 -0800

    remove unused code

--- launchdarkly/helper.go
+++ launchdarkly/helper.go
@@ -5,15 +5,6 @@ import (
 )
 
 func ptr(v interface{}) *interface{} { return &v }
-func stringPtr(v string) *string     { return &v }
-
-func stringList(v []interface{}) []string {
-	list := make([]string, len(v))
-	for i, elem := range v {
-		list[i] = elem.(string)
-	}
-	return list
-}
 
 func patchReplace(path string, value interface{}) ldapi.PatchOperation {
 	return ldapi.PatchOperation{
