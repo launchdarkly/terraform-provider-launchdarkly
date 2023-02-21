98b5c1a30b841c8a2c6cc7765e6dcd154f7f25da
Fabian <feldbergf@gmail.com>
2023-02-21 14:10:31 +0000

    [sc 189042] update project releaser config permissions (#277)
    * new config attempts
    
    * update sourcing for github token
    
    * generate key and passphrase for testing
    
    * set permissions on scripts
    
    * change scripts slightly to hopefully pick up permission changes
    
    * no sudo
    
    * no sudo and apt-get instead of apt
    
    * skip install prompts, simplify install
    
    * check secret env stuff
    
    * dude wheres my env
    
    * ?
    
    * no more env stuff?
    
    * set github token as passthrough as well
    
    * ?
    
    * get me the git history
    
    * snapshot it for now
    
    * one more env var
    
    * some more artifacts attempts
    
    * try getting tags and not using snapshot
    
    * try getting tags and not using snapshot
    
    * try new way of getting env vars in goreleaser yml
    
    * leave git tagging for now
    
    * fix typo
    
    * test
    
    * hardcode dist for goreleaser
    
    * remove snapshot arg
    
    * move artifact creation to publish step to take advantage of new tag
    
    * stop goreleaser from creating a release draft
    
    * update build script
    
    * add dry run script, remove release notes from goreleaser, re-enable goreleaser in build step to test
    
    * build script is now divided into prepare and publish
    
    * update dry run script permissions
    
    * remove release action workflow
    
    * add newlines
    
    * cleanup
    
    * permissioning for scripts
    
    * add update-version script
    
    * move update-version file
    
    * update update script
    
    * update update script
    
    * update update script
    
    * update update script
    
    * update update script
    
    * update update script
    
    * update update script
    
    * fix changelog
    
    * add newline
    
    * remove extra newline

--- .ldrelease/publish.sh
+++ .ldrelease/publish.sh
@@ -3,5 +3,5 @@
 set -ue
 
 # Run goreleaser
-# We can't run in the build step, as project-releaser only tags the commit after the build step finishes, and goreleaser pulls the tag off the most recent commit
+# We can't run in the build step, as project-releaser only tags the commit after the build step finishes and goreleaser pulls the tag off the most recent commit
 GPG_FINGERPRINT=$(gpg --with-colons --list-keys | awk -F: '/^pub/ { print $5 }') GITHUB_TOKEN="$(cat "${LD_RELEASE_SECRETS_DIR}/github_token")" goreleaser release --clean --release-notes ../entry.tmp
