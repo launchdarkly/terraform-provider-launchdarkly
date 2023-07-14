402f8e4305dd1846ae4d5238d546010a1d919bb5
Henry Barrow <hbarrow@launchdarkly.com>
2023-07-14 14:43:25 +0100

    Add support for future terraform-plugin-framework resources using tf5muxserver (#302)
    * Update main.go to use tf5muxserver
    
    * Add support for plugin-framework
    
    * update provider config
    
    * Remove commented out code

--- launchdarkly/provider.go
+++ launchdarkly/provider.go
@@ -3,10 +3,15 @@ package launchdarkly
 import (
 	"context"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+const (
+	DEFAULT_LAUNCHDARKLY_HOST = "https://app.launchdarkly.com"
 )
 
 // Environment Variables
@@ -23,29 +28,30 @@ const (
 	api_host     = "api_host"
 )
 
+func providerSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		access_token: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The LaunchDarkly API key",
+		},
+		oauth_token: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The LaunchDarkly OAuth token",
+		},
+		api_host: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The LaunchDarkly host address, e.g. https://app.launchdarkly.com",
+		},
+	}
+}
+
 // Provider returns a *schema.Provider.
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			access_token: {
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc(LAUNCHDARKLY_ACCESS_TOKEN, nil),
-				Description: "The LaunchDarkly API key",
-			},
-			oauth_token: {
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc(LAUNCHDARKLY_OAUTH_TOKEN, nil),
-				Description: "The LaunchDarkly OAuth token",
-			},
-			api_host: {
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc(LAUNCHDARKLY_API_HOST, "https://app.launchdarkly.com"),
-				Description: "The LaunchDarkly host address, e.g. https://app.launchdarkly.com",
-			},
-		},
+		Schema: providerSchema(),
 		ResourcesMap: map[string]*schema.Resource{
 			"launchdarkly_team":                      resourceTeam(),
 			"launchdarkly_project":                   resourceProject(),
@@ -84,14 +90,35 @@ func Provider() *schema.Provider {
 
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 	var diags diag.Diagnostics
-	host := d.Get(api_host).(string)
+
+	// check environment variables first
+	accessToken := os.Getenv(LAUNCHDARKLY_ACCESS_TOKEN)
+	oauthToken := os.Getenv(LAUNCHDARKLY_OAUTH_TOKEN)
+	host := os.Getenv(LAUNCHDARKLY_API_HOST)
+
+	if host == "" {
+		host = DEFAULT_LAUNCHDARKLY_HOST
+	}
+	configHost := d.Get(api_host).(string)
+	if configHost != "" {
+		host = configHost
+	}
+
 	if strings.HasPrefix(host, "http") {
 		u, _ := url.Parse(host)
 		host = u.Host
 	}
-	accessToken := d.Get(access_token).(string)
-	oauthToken := d.Get(oauth_token).(string)
 
+	// Check configuration data, which should take precedence over
+	// environment variable data, if found.
+	configAccessToken := d.Get(access_token).(string)
+	if configAccessToken != "" {
+		accessToken = configAccessToken
+	}
+	configOAuthToken := d.Get(oauth_token).(string)
+	if configOAuthToken != "" {
+		oauthToken = configOAuthToken
+	}
 	if oauthToken == "" && accessToken == "" {
 		return nil, diag.Errorf("either an %q or %q must be specified", access_token, oauth_token)
 	}
