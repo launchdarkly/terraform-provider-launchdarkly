e550f5a2471c54aaf774b33402e1eda911fad7fc
Henry Barrow <hbarrow@launchdarkly.com>
2023-07-18 09:26:35 +0100

    Extend GPG passphrase cache TTL to fix release pipeline (#308)

--- .ldrelease/publish-dry-run.sh
+++ .ldrelease/publish-dry-run.sh
@@ -4,7 +4,7 @@ set -ue
 
 # Run goreleaser
 # We can't run in the build step, as project-releaser only tags the commit after the build step finishes and goreleaser pulls the tag off the most recent commit
-GPG_TTY=$(tty) GPG_FINGERPRINT=$(gpg --with-colons --list-keys | awk -F: '/^pub/ { print $5 }') GITHUB_TOKEN="$(cat "${LD_RELEASE_SECRETS_DIR}/github_token")" LD_RELEASE_ARTIFACTS_DIR="${LD_RELEASE_ARTIFACTS_DIR}" goreleaser release --debug --clean --snapshot
+GPG_FINGERPRINT=$(gpg --with-colons --list-keys | awk -F: '/^pub/ { print $5 }') GITHUB_TOKEN="$(cat "${LD_RELEASE_SECRETS_DIR}/github_token")" LD_RELEASE_ARTIFACTS_DIR="${LD_RELEASE_ARTIFACTS_DIR}" goreleaser release --clean --snapshot
 
 # Remove extra files that we don't want in our release
 rm /tmp/project-releaser/artifacts/artifacts.json
