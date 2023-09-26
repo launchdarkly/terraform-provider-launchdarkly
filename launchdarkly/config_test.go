13003208855a8784d131bb6cdb3f93814c9146ea
Henry Barrow <hbarrow@launchdarkly.com>
2023-09-26 18:02:02 +0100

    Add 404 retries to team role mapping resource (#329)
    * Add 404 retry client and tests
    
    * Use 404 client in more places
    
    * Increase test timeout

--- launchdarkly/config_test.go
+++ launchdarkly/config_test.go
@@ -209,3 +209,74 @@ func TestHandleConflicts(t *testing.T) {
 		assert.Equal(t, 3, calls)
 	})
 }
+
+func Test404RetryClient(t *testing.T) {
+	t.Run("no retries needed", func(t *testing.T) {
+		t.Parallel()
+		calls := 0
+
+		// create a test server
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			calls++
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer ts.Close()
+
+		// create a client
+		client, err := newClient("token", ts.URL, false, DEFAULT_HTTP_TIMEOUT_S)
+		require.NoError(t, err)
+
+		res, err := client.ld404Retry.GetConfig().HTTPClient.Get(ts.URL)
+		require.NoError(t, err)
+		assert.Equal(t, res.StatusCode, http.StatusOK)
+		assert.Equal(t, calls, 1)
+	})
+
+	t.Run("max retries exceeded", func(t *testing.T) {
+		t.Parallel()
+		calls := 0
+
+		// create a test server
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			calls++
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer ts.Close()
+
+		// create a client
+		client, err := newClient("token", ts.URL, false, DEFAULT_HTTP_TIMEOUT_S)
+		require.NoError(t, err)
+
+		res, err := client.ld404Retry.GetConfig().HTTPClient.Get(ts.URL)
+		require.NoError(t, err)
+		assert.Equal(t, res.StatusCode, http.StatusNotFound)
+		assert.Equal(t, calls, MAX_RETRIES+1)
+	})
+
+	t.Run("Resource found after a few retries", func(t *testing.T) {
+		t.Parallel()
+		calls := 0
+
+		// create a test server
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			calls++
+
+			if calls == 3 {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer ts.Close()
+
+		// create a client
+		client, err := newClient("token", ts.URL, false, DEFAULT_HTTP_TIMEOUT_S)
+		require.NoError(t, err)
+
+		res, err := client.ld404Retry.GetConfig().HTTPClient.Get(ts.URL)
+		require.NoError(t, err)
+		assert.Equal(t, res.StatusCode, http.StatusOK)
+		assert.Equal(t, 3, calls)
+	})
+}
