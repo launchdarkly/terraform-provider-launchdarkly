52a2e6dc4cdbdc0911a335e302193392c84c0cdb
Isabelle Miller <imiller@launchdarkly.com>
2025-02-06 12:43:38 +0100

    update scheam def

--- launchdarkly/role_attributes_helper.go
+++ launchdarkly/role_attributes_helper.go
@@ -4,10 +4,23 @@ import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 func roleAttributesSchema(isDataSource bool) *schema.Schema {
 	return &schema.Schema{
-		Type: schema.TypeMap,
-		Elem: &schema.Schema{
-			Type: schema.TypeList,
-			Elem: &schema.Schema{Type: schema.TypeString},
+		Type: schema.TypeList,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				KEY: {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "The key / name of your role attribute. In the example `$${roleAttribute/testAttribute}`, the key is `testAttribute`.",
+				},
+				VALUES: {
+					Type: schema.TypeList,
+					Elem: &schema.Schema{
+						Type: schema.TypeString,
+					},
+					Required:    true,
+					Description: "A list of values for your role attribute. For example, if your policy statement defines the resource `\"proj/$${roleAttribute/testAttribute}\"`, the values would be the keys of the projects you wanted to assign access to.",
+				},
+			},
 		},
 		Optional:    true,
 		Computed:    isDataSource,
