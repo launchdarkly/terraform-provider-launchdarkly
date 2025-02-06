fe26aec9cb8a591770591433023955ae1a5db1cc
Isabelle Miller <imiller@launchdarkly.com>
2025-02-06 12:22:30 +0100

    update schema to specify map elems

--- launchdarkly/role_attributes_helper.go
+++ launchdarkly/role_attributes_helper.go
@@ -4,7 +4,11 @@ import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 func roleAttributesSchema(isDataSource bool) *schema.Schema {
 	return &schema.Schema{
-		Type:        schema.TypeMap,
+		Type: schema.TypeMap,
+		Elem: &schema.Schema{
+			Type: schema.TypeList,
+			Elem: &schema.Schema{Type: schema.TypeString},
+		},
 		Optional:    true,
 		Computed:    isDataSource,
 		Description: "A map of role attributes. The key is the role attribute key and the value is a string array of resource keys that apply.",
