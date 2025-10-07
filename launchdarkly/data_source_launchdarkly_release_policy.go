5162e5e43819366525217e9c3108cc7521f31ee8
Lucy <thelucyvoigt@gmail.com>
2025-10-07 14:47:54 -0700

    feat: [RO-2042] Add release policy datasource (#355)
    * [RO-2042] Add release policy datasource
    
    Pretty straightforward - uses the 'read' function implemented for the
    resource, similar to other datasources in the repo.
    
    * Update examples/data-sources/launchdarkly_release_policy/data-source.tf
    
    Co-authored-by: Drew Inglis <drew@drewinglis.com>
    
    ---------
    
    Co-authored-by: Drew Inglis <drew@drewinglis.com>

---
+++ launchdarkly/data_source_launchdarkly_release_policy.go
@@ -0,0 +1,83 @@
+package launchdarkly
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceReleasePolicy() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceReleasePolicyRead,
+
+		Description: `Provides a LaunchDarkly release policy data source. This data source is still in beta.
+
+This data source allows you to retrieve release policy information from your LaunchDarkly organization.
+
+Learn more about [release policies here](https://launchdarkly.com/docs/home/releases/release-policies), and read our [API docs here](https://launchdarkly.com/docs/api/release-policies-beta/).`,
+
+		Schema: map[string]*schema.Schema{
+			PROJECT_KEY: {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The project key.",
+			},
+			KEY: {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The release policy's unique key.",
+			},
+			NAME: {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The release policy's name.",
+			},
+			RELEASE_METHOD: {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The release method for the release policy.",
+			},
+			SCOPE: {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "The scope configuration for the release policy.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						SCOPE_ENVIRONMENT_KEYS: {
+							Type:        schema.TypeList,
+							Computed:    true,
+							Description: "The environment keys for environments the release policy is applied to.",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
+			},
+			GUARDED_RELEASE_CONFIG: {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "Configuration for guarded release.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						ROLLBACK_ON_REGRESSION: {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: "Whether to automatically rollback on regression.",
+						},
+						MIN_SAMPLE_SIZE: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "The minimum sample size for the release policy.",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceReleasePolicyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return releasePolicyRead(ctx, d, meta, true)
+}
