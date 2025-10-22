826ee3a9733052d1ca0274965e1f9007ce650736
Fabian <ffeldberg@launchdarkly.com>
2025-10-22 16:39:53 +0100

    add some tests

---
+++ launchdarkly/resource_launchdarkly_feature_flag_view_keys_test.go
@@ -0,0 +1,297 @@
+package launchdarkly
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testAccFeatureFlagWithViewKeysCreate = `
+resource "launchdarkly_project" "test" {
+	name = "%s"
+	key  = "%s"
+}
+
+resource "launchdarkly_view" "view1" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-1"
+	name        = "Test View 1"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_view" "view2" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-2"
+	name        = "Test View 2"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_feature_flag" "test" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-flag-with-views"
+	name        = "Test Flag with Views"
+	variation_type = "boolean"
+	
+	view_keys = [
+		launchdarkly_view.view1.key,
+		launchdarkly_view.view2.key
+	]
+	
+	tags = ["test"]
+}
+`
+
+	testAccFeatureFlagWithViewKeysUpdate = `
+resource "launchdarkly_project" "test" {
+	name = "%s"
+	key  = "%s"
+}
+
+resource "launchdarkly_view" "view1" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-1"
+	name        = "Test View 1"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_view" "view2" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-2"
+	name        = "Test View 2"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_view" "view3" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-3"
+	name        = "Test View 3"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_feature_flag" "test" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-flag-with-views"
+	name        = "Test Flag with Views"
+	variation_type = "boolean"
+	
+	view_keys = [
+		launchdarkly_view.view1.key,
+		launchdarkly_view.view3.key
+	]
+	
+	tags = ["test"]
+}
+`
+
+	testAccFeatureFlagWithViewKeysRemoved = `
+resource "launchdarkly_project" "test" {
+	name = "%s"
+	key  = "%s"
+}
+
+resource "launchdarkly_view" "view1" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-1"
+	name        = "Test View 1"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_view" "view2" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-2"
+	name        = "Test View 2"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_view" "view3" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-3"
+	name        = "Test View 3"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_feature_flag" "test" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-flag-with-views"
+	name        = "Test Flag with Views"
+	variation_type = "boolean"
+	
+	tags = ["test"]
+}
+`
+
+	testAccFeatureFlagWithViewKeysNonexistentView = `
+resource "launchdarkly_project" "test" {
+	name = "%s"
+	key  = "%s"
+}
+
+resource "launchdarkly_view" "view1" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-view-1"
+	name        = "Test View 1"
+	maintainer_id = "%s"
+}
+
+resource "launchdarkly_feature_flag" "test" {
+	project_key = launchdarkly_project.test.key
+	key         = "test-flag-bad-view"
+	name        = "Test Flag with Bad View"
+	variation_type = "boolean"
+	
+	view_keys = [
+		launchdarkly_view.view1.key,
+		"nonexistent-view"
+	]
+}
+`
+)
+
+func TestAccFeatureFlagViewKeys_CreateAndUpdate(t *testing.T) {
+	accTest := os.Getenv("TF_ACC")
+	if accTest == "" {
+		t.SkipNow()
+	}
+
+	projectKey := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	projectName := "flag-view-keys-test-" + projectKey
+	resourceName := "launchdarkly_feature_flag.test"
+
+	client, err := newClient(os.Getenv(LAUNCHDARKLY_ACCESS_TOKEN), os.Getenv(LAUNCHDARKLY_API_HOST), false, DEFAULT_HTTP_TIMEOUT_S, DEFAULT_MAX_CONCURRENCY)
+	require.NoError(t, err)
+
+	members, _, err := client.ld.AccountMembersApi.GetMembers(client.ctx).Execute()
+	require.NoError(t, err)
+	require.True(t, len(members.Items) > 0, "This test requires at least one member in the account")
+
+	maintainerId := members.Items[0].Id
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccFeatureFlagWithViewKeysCreate, projectName, projectKey, maintainerId, maintainerId),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckFeatureFlagExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "view_keys.#", "2"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "view_keys.*", "test-view-1"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "view_keys.*", "test-view-2"),
+					resource.TestCheckResourceAttr(resourceName, "linked_views.#", "2"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "linked_views.*", "test-view-1"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "linked_views.*", "test-view-2"),
+					// Verify via API that the flag is actually linked to the views
+					testAccCheckFlagLinkedToViews(projectKey, "test-flag-with-views", []string{"test-view-1", "test-view-2"}),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: fmt.Sprintf(testAccFeatureFlagWithViewKeysUpdate, projectName, projectKey, maintainerId, maintainerId, maintainerId),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckFeatureFlagExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "view_keys.#", "2"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "view_keys.*", "test-view-1"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "view_keys.*", "test-view-3"),
+					resource.TestCheckResourceAttr(resourceName, "linked_views.#", "2"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "linked_views.*", "test-view-1"),
+					resource.TestCheckTypeSetElemAttr(resourceName, "linked_views.*", "test-view-3"),
+					// Verify via API that view-2 was unlinked and view-3 was linked
+					testAccCheckFlagLinkedToViews(projectKey, "test-flag-with-views", []string{"test-view-1", "test-view-3"}),
+				),
+			},
+			{
+				Config: fmt.Sprintf(testAccFeatureFlagWithViewKeysRemoved, projectName, projectKey, maintainerId, maintainerId, maintainerId),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckFeatureFlagExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "view_keys.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "linked_views.#", "0"),
+					// Verify via API that all views were unlinked
+					testAccCheckFlagLinkedToViews(projectKey, "test-flag-with-views", []string{}),
+				),
+			},
+		},
+	})
+}
+
+func TestAccFeatureFlagViewKeys_NonexistentView(t *testing.T) {
+	accTest := os.Getenv("TF_ACC")
+	if accTest == "" {
+		t.SkipNow()
+	}
+
+	projectKey := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	projectName := "flag-bad-view-test-" + projectKey
+
+	client, err := newClient(os.Getenv(LAUNCHDARKLY_ACCESS_TOKEN), os.Getenv(LAUNCHDARKLY_API_HOST), false, DEFAULT_HTTP_TIMEOUT_S, DEFAULT_MAX_CONCURRENCY)
+	require.NoError(t, err)
+
+	members, _, err := client.ld.AccountMembersApi.GetMembers(client.ctx).Execute()
+	require.NoError(t, err)
+	require.True(t, len(members.Items) > 0, "This test requires at least one member in the account")
+
+	maintainerId := members.Items[0].Id
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      fmt.Sprintf(testAccFeatureFlagWithViewKeysNonexistentView, projectName, projectKey, maintainerId),
+				ExpectError: regexp.MustCompile("view does not exist"),
+			},
+		},
+	})
+}
+
+// testAccCheckFlagLinkedToViews verifies that a flag is linked to specific views via API
+func testAccCheckFlagLinkedToViews(projectKey, flagKey string, expectedViewKeys []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*Client)
+		betaClient, err := newBetaClient(client.apiKey, client.apiHost, false, DEFAULT_HTTP_TIMEOUT_S, DEFAULT_MAX_CONCURRENCY)
+		if err != nil {
+			return fmt.Errorf("failed to create beta client: %v", err)
+		}
+
+		// Get views containing this flag
+		viewKeys, err := getViewsContainingFlag(betaClient, projectKey, flagKey)
+		if err != nil {
+			return fmt.Errorf("failed to get views for flag: %v", err)
+		}
+
+		// Verify the exact set of views matches expectations
+		if len(viewKeys) != len(expectedViewKeys) {
+			return fmt.Errorf("expected flag to be linked to %d views, but got %d. Expected: %v, Actual: %v",
+				len(expectedViewKeys), len(viewKeys), expectedViewKeys, viewKeys)
+		}
+
+		// Check that all expected views are present
+		expectedSet := make(map[string]bool)
+		for _, view := range expectedViewKeys {
+			expectedSet[view] = true
+		}
+
+		for _, view := range viewKeys {
+			if !expectedSet[view] {
+				return fmt.Errorf("unexpected view %q found for flag. Expected: %v, Actual: %v",
+					view, expectedViewKeys, viewKeys)
+			}
+		}
+
+		return nil
+	}
+}
