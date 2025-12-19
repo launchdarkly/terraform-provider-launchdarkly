051253a8028cb99d844888b4e049d92949952222
Traci Siebel <traci@launchdarkly.com>
2025-12-19 12:17:29 -0800

    test

--- launchdarkly/resource_launchdarkly_feature_flag_environment_test.go
+++ launchdarkly/resource_launchdarkly_feature_flag_environment_test.go
@@ -1295,8 +1295,7 @@ func testAccCheckFeatureFlagEnvironmentDefaults(t *testing.T, projectKey, flagKe
 		require.NoError(t, err)
 		flag, _, err := client.ld.FeatureFlagsApi.GetFeatureFlag(client.ctx, projectKey, flagKey).Execute()
 		require.NoError(t, err)
-		require.NotNil(t, flag.Environments, "flag.Environments should not be nil")
-		envConfig := flag.Environments["test"]
+		envConfig, ok := flag.Environments["test"]
 		require.Equal(t, int32(0), *envConfig.OffVariation)
 		return nil
 	}
