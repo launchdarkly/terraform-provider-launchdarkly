e23c31aafcbcd5450a24a983869a9c9190780f68
Christopher Tarquini <ctarquini@launchdarkly.com>
2026-02-19 13:59:25 -0500

    chore: fix formatting

--- launchdarkly/approvals_helper_test.go
+++ launchdarkly/approvals_helper_test.go
@@ -383,4 +383,3 @@ func TestApprovalSettingFromMap_MultipleFieldErrorsWithSegment(t *testing.T) {
 	// Should get error for service_kind since it's checked first
 	assert.Contains(t, err.Error(), "service_kind cannot be set for resource_kind 'segment'")
 }
-
